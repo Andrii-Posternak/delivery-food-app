816bd8bd41115dd153f0380386e8f932377bb49c
Andrii-Posternak <panposternak@gmail.com>
2023-05-30 22:37:35 +0300

    added notifications

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,20 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
-import { Loader } from 'components/Loader/Loader';
 
 export const SharedLayout = () => {
   return (
     <>
       <Header />
       <main>
-        <Suspense
-          fallback={
-            <div>
-              <Loader />
-            </div>
-          }
-        >
+        <Suspense fallback={<div>Loading...</div>}>
           <Outlet />
         </Suspense>
       </main>
