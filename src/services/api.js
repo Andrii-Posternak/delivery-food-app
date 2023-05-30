f3d6a91e30074432a9d18eea540b10a6619627c1
Andrii-Posternak <panposternak@gmail.com>
2023-05-30 17:36:50 +0300

    changed baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3010/api';
+axios.defaults.baseURL = 'https://delivery-food-app-backend.onrender.com/api';
 
 export const getShopsApi = async () => {
   try {
